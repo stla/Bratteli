dd5e7bc5560b40cedee6c0be61d3f71bfd3c8ec1
stla <laurent_step@yahoo.fr>
2016-02-13 21:44:16 +0100

    chacon

--- Chacon/ChaconZnXn.R
+++ Chacon/ChaconZnXn.R
@@ -3,7 +3,7 @@ source('~/Work/Filtrations/Bratteli/Chacon/ChaconTransfo.R')
 # but : process Zn pour T=Chacon
 
 Zn2Wn <- function(z, n){ # z sous forme chemin
-  dn <- (3^(n+2)-1)/2
+  dn <- (3^(n)-1)/2
   Wn <- character(dn)
   Wn[1] <- ifelse(z[1] %in% c(2L,4L), "b", "a")
   for(i in 2:dn){
@@ -14,12 +14,12 @@ Zn2Wn <- function(z, n){ # z sous forme chemin
 }
 
 z <- reps()
-Zn2Wn(z, 1)
+Zn2Wn(z, 2)
 
 # Zn2Xn 
 Zn2Xn <- function(z,n){
   B <- FALSE
-  for(i in 1:(n+1)){
+  for(i in 1:(n-1)){
     B <- c(B, B, TRUE, B)
   }
   Wn <- Zn2Wn(z,n)
@@ -28,6 +28,25 @@ Zn2Xn <- function(z,n){
 }
 
 z <- reps()
-Zn2Xn(z, 1)
+Zn2Xn(z, 2)
 
-# simulation (V_n, eps_n)
+# simulations Wn 
+n <- 4
+nsims <- 1500
+Wn <- character(nsims)
+for(i in 1:nsims){
+  z <- reps()
+  Wn[i] <- paste0(Zn2Wn(z, n), collapse="")
+}
+length(unique(Wn)) # 7 pour n=2, 25 pour n=3, 79 pour n=4 : on trouve 3^n-2
+
+n <- 4
+nsims <- 1500
+Xn <- character(nsims)
+for(i in 1:nsims){
+  z <- reps()
+  Xn[i] <- paste0(Zn2Xn(z, n), collapse="")
+}
+length(unique(Xn)) # 6 pour n=2, 23 pour n=3, 77 pour n=4
+
+
